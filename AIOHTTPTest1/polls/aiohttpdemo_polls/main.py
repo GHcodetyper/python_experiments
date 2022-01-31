e11651137b23295c0709a1a758146417579aaf10
Vadim Buga <vadim.buga@esempla.com>
2022-01-31 23:09:36 +0200

    20220131 aiohttp

--- AIOHTTPTest1/polls/aiohttpdemo_polls/main.py
+++ AIOHTTPTest1/polls/aiohttpdemo_polls/main.py
@@ -3,9 +3,10 @@ from aiohttp import web
 
 from settings import config
 from routes import setup_routes
+from db import pg_context
 
 app = web.Application()
-setup_routes(app)
 app['config'] = config
-
+setup_routes(app)
+app.cleanup_ctx.append(pg_context)
 web.run_app(app)
